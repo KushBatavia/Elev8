ab78b18d09cda303f4a63bbe9d403de20440c42f
ComputerGeek-07 <atishayagupta07@gmail.com>
2025-02-03 20:09:04 +0530

    Limelight Stupid Stuff

--- src/main/java/frc/robot/subsystems/LimelightPoseEstimation.java
+++ src/main/java/frc/robot/subsystems/LimelightPoseEstimation.java
@@ -66,7 +66,6 @@ public class LimelightPoseEstimation extends SubsystemBase {
     //     pose,
     //     stateStdDevs,
     //     visionMeasurementStdDevs);
-    // TESTING THIS COMMENT SO THAT I CAN PUSH AND HAVE A NICE FRC SEASON IN LIFE YAY LETSGO
     SmartDashboard.putData("Field", field2d);
   }
 
