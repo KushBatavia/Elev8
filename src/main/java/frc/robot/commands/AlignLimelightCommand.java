335e703d0002ed1653285b100a6d595d629b25f3
ShadowPhoenix007 <96228746+ShadowPhoenix007@users.noreply.github.com>
2025-02-07 17:09:39 +0530

    edited version aligns only x rotation

--- src/main/java/frc/robot/commands/AlignLimelightCommand.java
+++ src/main/java/frc/robot/commands/AlignLimelightCommand.java
@@ -9,52 +9,36 @@ import frc.robot.LimelightHelpers;
 
 public class AlignLimelightCommand extends Command {
     private final CommandSwerveDrivetrain drivetrain;
-    private boolean alignX, alignY, alignTheta;
-    private double tx, ty, targetYaw, robotYaw;
+    private boolean alignX;
+    private double tx;
     private boolean returnFlag = false;
     private final PIDController xController = new PIDController(0.1, 0, 0);
-    private final PIDController yController = new PIDController(0.1, 0, 0);
-    private final PIDController thetaController = new PIDController(0.05, 0, 0); //Tune all of these later
-    private final Pigeon2 pigeon = new Pigeon2(0); //Double check CAN Ids later
-    private double offsetX;
 
 
     @Override
     public void initialize() {
+        alignX = true;
         returnFlag = false;
     }
 
-    public AlignLimelightCommand(CommandSwerveDrivetrain drivetrain, boolean alignX, boolean alignY, boolean alignTheta, double offsetX) {
+    public AlignLimelightCommand(CommandSwerveDrivetrain drivetrain) {
         this.drivetrain = drivetrain;
-        this.alignX = alignX;
-        this.alignY = alignY;
-        this.alignTheta = alignTheta;
-        this.offsetX = offsetX;
-        addRequirements(drivetrain);
     }
 
     @Override
     public void execute() {
-        if (!alignTheta) { robotYaw = 0; targetYaw = 0;}
-        else { targetYaw = LimelightHelpers.getBotPose2d_wpiBlue("limelight-new").getRotation().getDegrees(); }
         if (!alignX) { tx = 0;} 
         else { tx = LimelightHelpers.getTX("limelight-new");}
-        if (!alignY) { ty = 0;} 
-        else {ty = LimelightHelpers.getTY("limelight-new");}
 
-        double thetaError = targetYaw;
-        double speedX = xController.calculate(tx, offsetX);
-        double speedY = yController.calculate(ty, 0);
-        double speedTheta = thetaController.calculate(thetaError, 0);
+        double speedX = xController.calculate(tx, 0);
 
         drivetrain.setControl(new SwerveRequest.RobotCentric()
             .withVelocityX(speedX) // Forward/Backward
-            .withVelocityY(-speedY) // Left/Right
-            .withRotationalRate(speedTheta) // Align rotation
         );
 
-        if (Math.abs(xController.getPositionError()) < 0.1 && Math.abs(yController.getPositionError()) < 0.1 &&  Math.abs(thetaController.getPositionError()) < 2.0)
+        if (Math.abs(xController.getPositionError()) < 2 /*degrees */)
         {
+            alignX = false;
             returnFlag = true;
         }
     }
